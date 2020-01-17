13ad4bd7144e551dab46dc444fd6a038c4f084e1
kris10cabrera <cabrera.kristen@gmail.com>
2020-01-17 16:33:09 -0500

    reorder

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ props.ending ? (
   <div className="ending">This is the end! <span role="image" aria-label="party" >🎉</span></div>
 ) : (
   <div className="App">
-    <Scene tags={props.tags} />
     <Story sceneText={props.sceneText} />
     <Choices choices={props.currentChoices} makeChoice={props.makeChoice} />
+    <Scene tags={props.tags} />
   </div>
 );
 
